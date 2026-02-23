db381aba1925a81cdfc47830dada38e4774b0574
Adam Murdoch <adammurdochofficial@gmail.com>
2026-02-23 18:17:29 +1100

    Tidy

--- server.js
+++ server.js
@@ -25,11 +25,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options(/.*/, cors(corsOptions));
 
-// Body parsing middleware
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// Health check route
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
